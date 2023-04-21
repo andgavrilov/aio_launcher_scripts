525945dc18ba1b13d144851ff69b913cd5e2773d
andgavrilov <and.gavrilov@gmail.com>
2023-04-21 13:23:03 +0300

    3

--- gen_index.sh
+++ gen_index.sh
@@ -10,12 +10,7 @@ for f in *.lua; do
     cat $f | tr -d "\r" | grep '^--\s*[a-zA-Z]*\s*=' | sed -e 's/\ *=\ */": /' -e 's/^--\ */ "/' -e 's/$/,/' | grep -v 'arguments_'
     echo -n " \"md5sum\": "\"
     md5sum $f | cut -d ' ' -f 1 | sed 's/$/"/'
-
-    if [[ $f = $last_file ]]; then
-        echo }
-    else
-        echo },
-    fi
+    echo },
 done
 
 echo ]
